15f2c0361bc4e690bedd3f2a4c0370cf1c640e79
danghr <danghr@shanghaitech.edu.cn>
2020-10-07 20:17:09 +0800

    Add fixed point

---
+++ threads/fixed-point.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include "threads/fixed-point.h"
+
+/* Convert an integer to a fixed point number */
+fixed_point
+convert_int_to_fp (int x)
+{
+  return x << FP_FRACTION_BITS;
+}
+
+/* Convert a fixed point number to an integer
+   (Rounding toward zero) */
+fixed_point
+convert_fp_to_int_zero (fixed_point x)
+{
+  return x >> FP_FRACTION_BITS;
+}
+
+/* Convert a fixed point number to an integer
+   (Rounding to nearest) */
+fixed_point
+convert_fp_to_int_nearest (fixed_point x)
+{
+  if (x >= 0)
+    return (x + (FP_FRACTION_BITS >> 2)) >> FP_FRACTION_BITS;
+  return (x - (FP_FRACTION_BITS >> 2)) >> FP_FRACTION_BITS;
+}
+
+/* Addition of two fixed point numbers */
+fixed_point
+fp_add (fixed_point a, fixed_point b)
+{
+  return a + b;
+}
+
+/* Addition of a fixed point number and an integer */
+fixed_point
+fp_add_int (fixed_point a, int b)
+{
+  return a + convert_int_to_fp (b);
+}
+
+/* Subtraction of two fixed point numbers */
+fixed_point
+fp_sub (fixed_point a, fixed_point b)
+{
+  return a - b;
+}
+
+/* Subtraction of a fixed point number and an integer */
+fixed_point
+fp_sub_int (fixed_point a, int b)
+{
+  return a - convert_int_to_fp (b);
+}
+
+/* Subtraction of two fixed point numbers */
+fixed_point
+fp_mul (fixed_point a, fixed_point b)
+{
+  return ((int64_t)a) * b >> FP_FRACTION_BITS;
+}
+
+/* Subtraction of a fixed point number and an integer */
+fixed_point
+fp_mul_int (fixed_point a, int b)
+{
+  return a * b;
+}
+
+/* Subtraction of two fixed point numbers */
+fixed_point
+fp_div (fixed_point a, fixed_point b)
+{
+  return ((int64_t)a) << FP_FRACTION_BITS / b;
+}
+
+/* Subtraction of a fixed point number and an integer */
+fixed_point
+fp_div_int (fixed_point a, int b)
+{
+  return a / b;
+}
---
+++ threads/fixed-point.h
@@ -0,0 +1,23 @@
+#ifndef THREADS_FIXED_POINT_H
+#define THREADS_FIXED_POINT_H
+
+#include <stdio.h>
+
+/* Definition of fixed-point numbers */
+typedef int fixed_point;
+
+#define FP_FRACTION_BITS 14
+
+fixed_point convert_int_to_fp (int x);
+fixed_point convert_fp_to_int_r0 (fixed_point x);
+fixed_point convert_fp_to_int_nearest (fixed_point x);
+fixed_point fp_add (fixed_point a, fixed_point b);
+fixed_point fp_add_int (fixed_point a, int b);
+fixed_point fp_sub (fixed_point a, fixed_point b);
+fixed_point fp_sub_int (fixed_point a, int b);
+fixed_point fp_mul (fixed_point a, fixed_point b);
+fixed_point fp_mul_int (fixed_point a, int b);
+fixed_point fp_div (fixed_point a, fixed_point b);
+fixed_point fp_div_int (fixed_point a, int b);
+
+#endif
