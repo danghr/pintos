9743a71662039d14e0d1d7b69249bf38909aa527
hhhhhty <zhangty2@shanghaitech.edu.cn>
2020-10-07 18:23:38 +0800

    Add the priority_compare in thread.h and change all the list_push_back to list_insert_order.

--- threads/thread.h
+++ threads/thread.h
@@ -139,5 +139,5 @@ int thread_get_nice (void);
 void thread_set_nice (int);
 int thread_get_recent_cpu (void);
 int thread_get_load_avg (void);
-
+bool thread_priority_compare(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED);
 #endif /* threads/thread.h */
