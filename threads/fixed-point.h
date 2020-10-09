edc19e872fb3fcefbb9dcdb5ee9ada09a56ce96d
danghr <danghr@shanghaitech.edu.cn>
2020-10-09 19:49:32 +0800

    Fix fixed-point.h

--- threads/fixed-point.h
+++ threads/fixed-point.h
@@ -9,7 +9,7 @@ typedef int fixed_point;
 #define FP_FRACTION_BITS 14
 
 fixed_point convert_int_to_fp (int x);
-fixed_point convert_fp_to_int_r0 (fixed_point x);
+fixed_point convert_fp_to_int_zero (fixed_point x);
 fixed_point convert_fp_to_int_nearest (fixed_point x);
 fixed_point fp_add (fixed_point a, fixed_point b);
 fixed_point fp_add_int (fixed_point a, int b);
