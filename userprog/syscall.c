0ddbe579ac8d51661c726417e937578f6cbbe1bf
danghr <danghr@shanghaitech.edu.cn>
2020-10-18 17:51:13 +0800

    Modify function declarations, add system call numbers & modify comment

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -9,6 +9,59 @@ static void syscall_handler (struct intr_frame *);
 /* Interrupt handler wrapper functions */
 static int (*syscall_handler_wrapper[20]) (struct intr_frame *);
 
+/* Projects 2 and later. */
+static void syscall_halt (void) NO_RETURN;
+static void syscall_exit (int) NO_RETURN;
+static pid_t syscall_exec (const char *);
+static int syscall_wait (pid_t);
+static bool syscall_create (const char *, unsigned);
+static bool syscall_remove (const char *);
+static int syscall_open (const char *);
+static int syscall_filesize (int);
+static int syscall_read (int, void *, unsigned);
+static int syscall_write (int, const void *, unsigned);
+static void syscall_seek (int, unsigned);
+static unsigned syscall_tell (int);
+static void syscall_close (int);
+
+/* Project 3 and optionally project 4. */
+static mapid_t syscall_mmap (int, void *);
+static void syscall_munmap (mapid_t);
+
+/* Project 4 only. */
+static bool syscall_chdir (const char *);
+static bool syscall_mkdir (const char *);
+static bool syscall_readdir (int, char[READDIR_MAX_LEN + 1]);
+static bool syscall_isdir (int);
+static int syscall_inumber (int);
+
+/* System call wrappers. */
+/* Projects 2 and later. */
+static int syscall_halt_wrapper (struct intr_frame *);
+static int syscall_exit_wrapper (struct intr_frame *);
+static int syscall_exec_wrapper (struct intr_frame *);
+static int syscall_wait_wrapper (struct intr_frame *);
+static int syscall_create_wrapper (struct intr_frame *);
+static int syscall_remove_wrapper (struct intr_frame *);
+static int syscall_open_wrapper (struct intr_frame *);
+static int syscall_filesize_wrapper (struct intr_frame *);
+static int syscall_read_wrapper (struct intr_frame *);
+static int syscall_write_wrapper (struct intr_frame *);
+static int syscall_seek_wrapper (struct intr_frame *);
+static int syscall_tell_wrapper (struct intr_frame *);
+static int syscall_close_wrapper (struct intr_frame *);
+
+/* Project 3 and optionally project 4. */
+static int syscall_mmap_wrapper (struct intr_frame *);
+static int syscall_munmap_wrapper (struct intr_frame *);
+
+/* Project 4 only. */
+static int syscall_chdir_wrapper (struct intr_frame *);
+static int syscall_mkdir_wrapper (struct intr_frame *);
+static int syscall_readdir_wrapper (struct intr_frame *);
+static int syscall_isdir_wrapper (struct intr_frame *);
+static int syscall_inumber_wrapper (struct intr_frame *);
+
 void
 syscall_init (void) 
 {
@@ -35,14 +88,48 @@ syscall_init (void)
   syscall_handler_wrapper[SYS_INUMBER] = &syscall_inumber_wrapper;
 }
 
+/* Return stack pointer */
+void *
+find_stack (struct intr_frame *f)
+{
+  return f->esp;
+}
+
+/* Kill the program which is violating the system */
+void
+terminate_program (int exit_status)
+{
+  thread_current ()->exit_status = exit_status;
+  thread_exit ();
+}
+
 /* System call handler.
    Retrieve the system call number and send it to correct
    wrappers. */
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  /* System call number is saved in stack pointer (f->esp).
+     See section 3.5.2 in the doc for details. */
+  int syscall_num = * (int*) find_stack (f);
+  int wrapper_return;
+
+  /* Check wheteher correct syscall num is correct */
+  if (syscall_num < 0 || syscall_num >= 20)
+    {
+      terminate_program (-1);
+    }
+  
+  /* Send it to correct wrapper to decode. 
+     If the wrapper returns a non-zero status, then we can 
+     detect that the user program is violating the system 
+     (memory access, etc.), so we need to terminate the 
+     program */
+  wrapper_return = syscall_handler_wrapper[syscall_num] (f);
+  if (wrapper_return != 0)
+    {
+      terminate_program (wrapper_return);
+    }
 }
 
 /* System calls. */
@@ -60,8 +147,7 @@ syscall_handler (struct intr_frame *f UNUSED)
    Also, all the arguments passed to system calls should be 
    checked, and with an illegal argument, we can returning an 
    error value (for those calls that return a value), returning 
-   an undefined value, or terminating the process.
-   */
+   an undefined value, or terminating the process. */
 
 /* Projects 2 and later. */
 
@@ -78,8 +164,7 @@ syscall_halt (void)
 static void
 syscall_exit (int status)
 {
-   thread_current ()->exit_status = status;
-   thread_exit ();
+   terminate_program (status);
 }
 
 /* Runs the executable whose name is given in CMD_LINE, passing 
@@ -184,11 +269,14 @@ syscall_close (int fd)
 
 }
 
-/* System call wrappers .
+/* System call wrappers.
    Retrive correct argument from the stack and send it to call 
    functions. 
-   Returns 0 if operation is correct, -1 if the operation fails 
-   or illegal. */
+
+   Returns 0 if operation is correct, otherwise returns -1. 
+   
+   Also, return value of system calls should be saved in f->eax.
+   See section 3.5.2 in the doc for details. */
 
 /* Projects 2 and later. */
 
--- userprog/syscall.h
+++ userprog/syscall.h
@@ -4,58 +4,8 @@
 #include "../lib/user/syscall.h"
 
 void syscall_init (void);
-
-/* Projects 2 and later. */
-static void syscall_halt (void) NO_RETURN;
-static void syscall_exit (int) NO_RETURN;
-static pid_t syscall_exec (const char *);
-static int syscall_wait (pid_t);
-static bool syscall_create (const char *, unsigned);
-static bool syscall_remove (const char *);
-static int syscall_open (const char *);
-static int syscall_filesize (int);
-static int syscall_read (int, void *, unsigned);
-static int syscall_write (int, const void *, unsigned);
-static void syscall_seek (int, unsigned);
-static unsigned syscall_tell (int);
-static void syscall_close (int);
-
-/* Project 3 and optionally project 4. */
-static mapid_t syscall_mmap (int, void *);
-static void syscall_munmap (mapid_t);
-
-/* Project 4 only. */
-static bool syscall_chdir (const char *);
-static bool syscall_mkdir (const char *);
-static bool syscall_readdir (int, char[READDIR_MAX_LEN + 1]);
-static bool syscall_isdir (int);
-static int syscall_inumber (int);
-
-/* System call wrappers. */
-/* Projects 2 and later. */
-static int syscall_halt_wrapper (struct intr_frame *);
-static int syscall_exit_wrapper (struct intr_frame *);
-static int syscall_exec_wrapper (struct intr_frame *);
-static int syscall_wait_wrapper (struct intr_frame *);
-static int syscall_create_wrapper (struct intr_frame *);
-static int syscall_remove_wrapper (struct intr_frame *);
-static int syscall_open_wrapper (struct intr_frame *);
-static int syscall_filesize_wrapper (struct intr_frame *);
-static int syscall_read_wrapper (struct intr_frame *);
-static int syscall_write_wrapper (struct intr_frame *);
-static int syscall_seek_wrapper (struct intr_frame *);
-static int syscall_tell_wrapper (struct intr_frame *);
-static int syscall_close_wrapper (struct intr_frame *);
-
-/* Project 3 and optionally project 4. */
-static int syscall_mmap_wrapper (struct intr_frame *);
-static int syscall_munmap_wrapper (struct intr_frame *);
-
-/* Project 4 only. */
-static int syscall_chdir_wrapper (struct intr_frame *);
-static int syscall_mkdir_wrapper (struct intr_frame *);
-static int syscall_readdir_wrapper (struct intr_frame *);
-static int syscall_isdir_wrapper (struct intr_frame *);
-static int syscall_inumber_wrapper (struct intr_frame *);
+void *validate_memory_access (uint32_t *, const void *);
+void terminate_program (int);
+void *find_stack (struct intr_frame *);
 
 #endif /* userprog/syscall.h */
