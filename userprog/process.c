eaa8596368fc358c47858fc367499be11ef6fc15
danghr <danghr@shanghaitech.edu.cn>
2020-10-24 23:31:34 +0800

    Formatting

--- userprog/process.c
+++ userprog/process.c
@@ -476,5 +476,6 @@ install_page(void *upage, void *kpage, bool writable)
 
   /* Verify that there's not already a page at that virtual
      address, then map our page there. */
-  return (pagedir_get_page(t->pagedir, upage) == NULL && pagedir_set_page(t->pagedir, upage, kpage, writable));
+  return (pagedir_get_page(t->pagedir, upage) == NULL && 
+    pagedir_set_page(t->pagedir, upage, kpage, writable));
 }
