c1446bd82787ffbccdedc9ffcdc027f3836a8b9e
danghr <danghr@shanghaitech.edu.cn>
2020-10-30 16:43:30 +0800

    Try to add a lock but it doesn't work

--- threads/thread.c
+++ threads/thread.c
@@ -481,6 +481,7 @@ init_thread (struct thread *t, const char *name, int priority)
   t->is_waited = false;
   t->is_waiting = false;
   t->executing_file = NULL;
+  t->exit_status = -1;
 }
 
 static void init_thread_child (struct thread* t, struct thread* parent)
--- userprog/process.h
+++ userprog/process.h
@@ -1,6 +1,8 @@
 #ifndef USERPROG_PROCESS_H
 #define USERPROG_PROCESS_H
 
+#include <list.h>
+#include "threads/synch.h"
 #include "threads/thread.h"
 
 #define MAX_CMD_LENGTH 100
@@ -13,4 +15,11 @@ void process_exit (void);
 void process_activate (void);
 void find_exec_name (const char *file_name, char *execute_name);
 void find_args (char *file_name, int *argc, char *argv[]);
+
+struct filename_lock
+{
+  char *filename;
+  struct lock lock;
+  struct list_elem elem;
+};
 #endif /* userprog/process.h */
