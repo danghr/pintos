0d5404b06c52d05df51c1811dee38a3facccabdc
danghr <danghr@outlook.com>
2020-11-25 14:07:24 +0800

    Adapt page fault in user mode

--- userprog/exception.c
+++ userprog/exception.c
@@ -161,12 +161,7 @@ page_fault (struct intr_frame *f)
           return;
         }
         else{
-          printf ("Page fault at %p: %s error %s page in %s context.\n",
-          fault_addr,
-          not_present ? "not present" : "rights violation",
-          write ? "writing" : "reading",
-          user ? "user" : "kernel");
-          kill(f);
+          terminate_program (-1);
       }
           
     }
@@ -191,12 +186,7 @@ page_fault (struct intr_frame *f)
     }
     else
     {
-      printf ("Page fault at %p: %s error %s page in %s context.\n",
-      fault_addr,
-      not_present ? "not present" : "rights violation",
-      write ? "writing" : "reading",
-      user ? "user" : "kernel");
-      kill (f);
+      terminate_program (-1);
     }
 
   }
