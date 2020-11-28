70debf6398a6cb740b1f325c56a0e21bcf1b71f9
danghr <danghr@outlook.com>
2020-11-28 14:20:32 +0000

    Adapt format

--- threads/thread.h
+++ threads/thread.h
@@ -137,22 +137,22 @@ struct thread
   fixed_point recent_cpu;             /* "recent_cpu" value of a thread. */
 
   /* List used to store its child threads */
-  struct list child_threads_list;    /* List of child threads */
-  struct list_elem child_elem;       /* Element of a child thread */
+  struct list child_threads_list;     /* List of child threads */
+  struct list_elem child_elem;        /* Element of a child thread */
 
-  struct list waiting;               /* Waiting list */
-  bool is_exited;                    /* Thread is exited */
-  bool is_waited;                    /* Thread is waited by others */
-  bool is_waiting;                   /* Thread is waiting for others */
-  struct thread* parent_thread;      /* Parent thread */
+  struct list waiting;                /* Waiting list */
+  bool is_exited;                     /* Thread is exited */
+  bool is_waited;                     /* Thread is waited by others */
+  bool is_waiting;                    /* Thread is waiting for others */
+  struct thread* parent_thread;       /* Parent thread */
 
   /* Supplemental page table */
-  struct list sup_page_table;         /* List of supplemental page tables */
-  struct lock sup_page_table_lock;    /* Lock of the list of SPT */
+  struct list sup_page_table;          /* List of supplemental page tables */
+  struct lock sup_page_table_lock;     /* Lock of the list of SPT */
 
   /* Memory-mapped files */
-  mapid_t next_mapid;                 /* Next identifier for mmap files */
-  struct list mapped_files;           /* List of memory-mapped files */
+  mapid_t next_mapid;                  /* Next identifier for mmap files */
+  struct list mapped_files;            /* List of memory-mapped files */
 };
 
 /* If false (default), use round-robin scheduler.
